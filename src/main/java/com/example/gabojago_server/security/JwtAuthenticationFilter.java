fc49de74cc200e50b67de4375f8ae55732f31491
syahn <496300@naver.com>
2023-03-28 14:31:00 +0900

    feat: jwt 토큰 관련 추가
    - JwtTokenProvider : 토큰 암호화, 복호화, 검증 로직
    - JwtAuthenticationFilter : 필터링
    로직

---
+++ src/main/java/com/example/gabojago_server/security/JwtAuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.example.gabojago_server.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+    private final JwtTokenProvider jwtTokenProvider;
+
+    //request header에서 토큰 정보 가져오기
+    private String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        //헤더가 존재하고 bearer로 시작하면 추출
+        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)){
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                         FilterChain filterChain) throws IOException, ServletException {
+        String token = resolveToken(request);
+
+        //토큰이 존재하고 유효하면 securityContext에 저장
+        if(StringUtils.hasText(token) && jwtTokenProvider.validateToken(token)){
+            Authentication auth = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
