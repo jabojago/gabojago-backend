135c4ed1e8d11c82929c69034731c1dc8fd10174
impati <impati0716@icloud.com>
2023-04-02 19:50:25 +0900

    rename : controller 를 web 패키지 하위로 이동(#4)

---
+++ src/main/java/com/example/gabojago_server/web/controller/member/AuthController.java
@@ -0,0 +1,30 @@
+package com.example.gabojago_server.web.controller.member;
+
+import com.example.gabojago_server.dto.TokenDto;
+import com.example.gabojago_server.dto.request.member.MemberRequestDto;
+import com.example.gabojago_server.dto.response.member.MemberResponseDto;
+import com.example.gabojago_server.service.member.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody @Valid MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.joinMember(requestDto));
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<TokenDto> login(TokenDto tokenDto) {
+        return ResponseEntity.ok(tokenDto);
+    }
+
+}
