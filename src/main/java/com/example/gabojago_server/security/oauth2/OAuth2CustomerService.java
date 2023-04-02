06a369e2d38683878d2ac697b2f8f98d1cb68bdd
impati <impati0716@icloud.com>
2023-04-02 19:56:02 +0900

    feat : oauth2 로그인 기능 구현(#4)

---
+++ src/main/java/com/example/gabojago_server/security/oauth2/OAuth2CustomerService.java
@@ -0,0 +1,42 @@
+package com.example.gabojago_server.security.oauth2;
+
+import com.example.gabojago_server.model.member.Authority;
+import com.example.gabojago_server.model.member.Member;
+import com.example.gabojago_server.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+@RequiredArgsConstructor
+public class OAuth2CustomerService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        KakaoMember kakaoMember = new KakaoMember(oAuth2User.getAttributes());
+
+        if (!memberRepository.existsByEmail(kakaoMember.getEmail())) {
+            Member member = Member.builder()
+                    .authority(Authority.ROLE_USER)
+                    .birth(kakaoMember.getBirthday())
+                    .email(kakaoMember.getEmail())
+                    .nickname(kakaoMember.getEmail())
+                    .name(kakaoMember.getName())
+                    .build();
+
+            memberRepository.save(member);
+        }
+        return new DefaultOAuth2User(oAuth2User.getAuthorities(), oAuth2User.getAttributes(), "id");
+    }
+
+}
