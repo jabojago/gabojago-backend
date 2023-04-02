cad15459bcb92bb211788ee0f540d3fe5c6427c9
impati <impati0716@icloud.com>
2023-04-02 19:52:47 +0900

    feat : token 을 발급하는 argumentResolver 등록(#4)

---
+++ src/main/java/com/example/gabojago_server/web/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.gabojago_server.web.config;
+
+import com.example.gabojago_server.jwt.JwtTokenProvider;
+import com.example.gabojago_server.web.argument.TokenArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new TokenArgumentResolver(jwtTokenProvider));
+    }
+
+}
