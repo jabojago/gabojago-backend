911714aae95b41659f106e3851dfb8dd7615fcd7
impati <impati0716@icloud.com>
2023-04-06 23:04:49 +0900

    feat : 동행 게시글 및 게시글 댓글 SAVE 테스트(#6)

--- src/main/java/com/example/gabojago_server/GabojagoServerApplication.java
+++ src/main/java/com/example/gabojago_server/GabojagoServerApplication.java
@@ -2,14 +2,12 @@ package com.example.gabojago_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class GabojagoServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GabojagoServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GabojagoServerApplication.class, args);
+    }
 
 }
