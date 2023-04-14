cb9c27ac208832f37c9b9d86741fc6ffe60a3468
syahn <496300@naver.com>
2023-04-15 03:09:48 +0900

    feat: 게시글 댓글 repository interface 생성(#8)

--- src/main/java/com/example/gabojago_server/repository/articlecomment/ArticleCommentRepository.java
+++ src/main/java/com/example/gabojago_server/repository/articlecomment/ArticleCommentRepository.java
@@ -1,7 +1,13 @@
 package com.example.gabojago_server.repository.articlecomment;
 
+import com.example.gabojago_server.model.article.Article;
 import com.example.gabojago_server.model.articlecomment.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+    List<ArticleComment> findAllByArticle(Article article);
 }
