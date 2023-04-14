b7303e746f00d08f7314232e945d14f9416d12c7
syahn <496300@naver.com>
2023-04-15 03:09:16 +0900

    feat: 게시글 댓글 controller 생성(#8)

---
+++ src/main/java/com/example/gabojago_server/web/controller/comment/CommentController.java
@@ -0,0 +1,40 @@
+package com.example.gabojago_server.web.controller.comment;
+
+import com.example.gabojago_server.dto.request.comment.CommentRequestDto;
+import com.example.gabojago_server.dto.response.comment.CommentResponseDto;
+import com.example.gabojago_server.service.comment.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<List<CommentResponseDto>> getAllComments(@PathVariable(value = "postId") Long postId) {
+        return ResponseEntity.ok(commentService.getAllComments(postId));
+    }
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<CommentResponseDto> postComment(@PathVariable(value = "postId") Long postId, @RequestBody CommentRequestDto request) {
+        return ResponseEntity.ok(commentService.createComment(postId, request.getContent()));
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<CommentResponseDto> changeComment(@PathVariable(value = "commentId") Long commentId, @RequestBody CommentRequestDto request) {
+        return ResponseEntity.ok(commentService.changeComment(commentId, request.getContent()));
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<String> deleteComment(@PathVariable(value = "commentId") Long commentId) {
+        commentService.removeComment(commentId);
+        return ResponseEntity.ok(new String("Success".getBytes(), StandardCharsets.UTF_8));
+    }
+}
