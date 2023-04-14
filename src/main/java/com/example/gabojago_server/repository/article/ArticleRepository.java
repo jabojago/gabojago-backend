137d53c6e5c634674cc5570bb76db9bc69b1bf76
syahn <496300@naver.com>
2023-04-15 02:58:55 +0900

    rename: AccompanyRepository에서 ArticleRepository로 이름 변경(#8)
    - repository 기능은 카테고리 상관없이 동일하므로 상위인 article로 이름 변경

--- src/main/java/com/example/gabojago_server/repository/article/ArticleRepository.java
+++ src/main/java/com/example/gabojago_server/repository/article/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.gabojago_server.repository.article;
 
 import com.example.gabojago_server.model.article.AccompanyArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AccompanyRepository extends JpaRepository<AccompanyArticle, Long> {
+@Repository
+public interface ArticleRepository extends JpaRepository<AccompanyArticle, Long>, ArticleRepositoryCustom {
 }
