84669deb7fb914d3a6fc499a01560c6b7736d5d1
syahn <496300@naver.com>
2023-04-15 02:22:01 +0900

    feat: 게시글 entity에 조회수 증가 메서드 및 댓글 리스트 추가(#8)
    - 조회시마다 조회수 증가 시키기 위해 조회수 증가 method를 추가
    - 댓글 리스트를 추가하고 게시글 삭제 시 댓글 삭제되도록 CascadeType.REMOVE 옵션 사용
    - writer의 fetch type을 lazy에서
    디폴트
    값인 eager로 수정

--- src/main/java/com/example/gabojago_server/model/article/Article.java
+++ src/main/java/com/example/gabojago_server/model/article/Article.java
@@ -1,11 +1,14 @@
 package com.example.gabojago_server.model.article;
 
+import com.example.gabojago_server.model.articlecomment.ArticleComment;
 import com.example.gabojago_server.model.common.BaseTimeEntity;
 import com.example.gabojago_server.model.member.Member;
 import lombok.Getter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,7 +21,7 @@ public abstract class Article extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     protected Member writer;
 
     @Column(nullable = false)
@@ -28,6 +31,13 @@ public abstract class Article extends BaseTimeEntity {
     protected String content; // 내용
 
     protected int review; //조회수
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    protected List<ArticleComment> commentList = new ArrayList<>();
+
+    public void reviewCountUp() {
+        this.review++;
+    }
 
     @Override
     public boolean equals(Object o) {
