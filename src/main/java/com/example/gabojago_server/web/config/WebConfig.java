a3f48d0b09f4c9ec2aab9216ab6536c50752d80a
impati <impati0716@icloud.com>
2023-04-19 17:39:49 +0900

    feat : cors 설정 (#10)

--- src/main/java/com/example/gabojago_server/web/config/WebConfig.java
+++ src/main/java/com/example/gabojago_server/web/config/WebConfig.java
@@ -19,5 +19,4 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new TokenArgumentResolver(jwtTokenProvider));
     }
-
 }
