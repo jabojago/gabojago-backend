0a1986ed6adac59b9295d207af6d2a7b24ef4a52
syahn <496300@naver.com>
2023-04-24 21:48:45 +0900

    feat: QnA 게시글 service 생성(#12)

---
+++ src/main/java/com/example/gabojago_server/service/article/QnaService.java
@@ -0,0 +1,36 @@
+package com.example.gabojago_server.service.article;
+
+import com.example.gabojago_server.dto.response.article.QnaResponseDto;
+import com.example.gabojago_server.repository.article.ArticleRepository;
+import com.example.gabojago_server.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class QnaService {
+    private final ArticleRepository articleRepository;
+    private final MemberRepository memberRepository;
+
+    public QnaResponseDto oneQna(Long writerId, Long articleId) {
+        return null;
+    }
+
+    @Transactional
+    public QnaResponseDto postQna(Long wirterId, String title, String content) {
+        return null;
+    }
+
+    @Transactional
+    public QnaResponseDto changeQnaArticle(Long writerId, Long articleId, String title,
+                                           String content, boolean isSelected) {
+        return null;
+    }
+
+    @Transactional
+    public void deleteQnaArticle(Long writerId, Long articleId) {
+
+    }
+}
