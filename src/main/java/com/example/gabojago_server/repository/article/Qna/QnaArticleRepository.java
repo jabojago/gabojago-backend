3e876139e0fcf8e703c9d071b2202aa138fbf5df
syahn <496300@naver.com>
2023-04-24 23:20:20 +0900

    feat: QnA 게시글 repository 생성(#12)

---
+++ src/main/java/com/example/gabojago_server/repository/article/Qna/QnaArticleRepository.java
@@ -0,0 +1,9 @@
+package com.example.gabojago_server.repository.article.Qna;
+
+import com.example.gabojago_server.model.article.QnaArticle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QnaArticleRepository extends JpaRepository<QnaArticle, Long> {
+}
