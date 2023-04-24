0ad3fa706626dc5f114cebc880a6e4102f85c798
syahn <496300@naver.com>
2023-04-24 21:54:47 +0900

    feat: Qna 게시글 isSelected column 추가(#12)
    - 답변 미완료/완료 상태 저장

--- src/main/java/com/example/gabojago_server/model/article/QnaArticle.java
+++ src/main/java/com/example/gabojago_server/model/article/QnaArticle.java
@@ -1,10 +1,13 @@
 package com.example.gabojago_server.model.article;
 
+import com.example.gabojago_server.model.common.BooleanToYNConverter;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
@@ -15,8 +18,8 @@ import javax.persistence.Entity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class QnaArticle extends Article {
 
-//    @Column(name = "isSelected")
-//    @Convert(converter = BooleanToYNConverter.class)
-//    private boolean isSelected;
+    @Column(name = "isSelected")
+    @Convert(converter = BooleanToYNConverter.class)
+    private boolean isSelected;
 
 }
