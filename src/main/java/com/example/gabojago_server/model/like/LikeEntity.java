eecbca59c2bf772583934e32786250e7c43625cb
impati <impati0716@icloud.com>
2023-04-27 17:24:34 +0900

    feat : 좋아요 기능 구현 및 테스트(#14)

---
+++ src/main/java/com/example/gabojago_server/model/like/LikeEntity.java
@@ -0,0 +1,38 @@
+package com.example.gabojago_server.model.like;
+
+import com.example.gabojago_server.model.article.Article;
+import com.example.gabojago_server.model.common.BaseTimeEntity;
+import com.example.gabojago_server.model.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table(name = "like_entity")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikeEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public LikeEntity(Article article, Member member) {
+        this.article = article;
+        this.member = member;
+    }
+}
+
