a48a42f7c6e51169a04c2e6115efaa0ed6933cd6
impati <impati0716@icloud.com>
2023-04-28 20:43:46 +0900

    refactor : Member 를 직접 반환에서 nickname 만 반환 하도록 변경 및 빌더 추가(#15)

--- src/main/java/com/example/gabojago_server/dto/request/article/AccompanyRequestDto.java
+++ src/main/java/com/example/gabojago_server/dto/request/article/AccompanyRequestDto.java
@@ -1,10 +1,17 @@
 package com.example.gabojago_server.dto.request.article;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AccompanyRequestDto {
     private String title;
 
@@ -12,9 +19,11 @@ public class AccompanyRequestDto {
 
     private String region;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
 
-    private int recruitNumber;
+    private int recruitMember;
 }
--- src/main/java/com/example/gabojago_server/dto/request/article/QnaRequestDto.java
+++ src/main/java/com/example/gabojago_server/dto/request/article/QnaRequestDto.java
@@ -1,8 +1,14 @@
 package com.example.gabojago_server.dto.request.article;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class QnaRequestDto {
     private String title;
 
