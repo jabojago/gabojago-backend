5b910b053d9732597953bf21ffb769e3091cc4fa
syahn <496300@naver.com>
2023-05-03 15:06:30 +0900

    refactor: 게시글 request dto에 AllArgsConstructor 추가(#15)
    - AllArgsConstructor 애노테이션 추가함
    
    #15

--- src/main/java/com/example/gabojago_server/dto/request/article/ArticleRequestDto.java
+++ src/main/java/com/example/gabojago_server/dto/request/article/ArticleRequestDto.java
@@ -1,11 +1,13 @@
 package com.example.gabojago_server.dto.request.article;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ArticleRequestDto {
     String title;
     String content;
