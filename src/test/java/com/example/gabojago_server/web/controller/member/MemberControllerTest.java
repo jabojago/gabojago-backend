3919bd367e86af0dc6cba47a91b8aea1c5ccec81
impati <impati0716@icloud.com>
2023-05-11 19:39:31 +0900

    test : 알람 기능 도입 테스트(#19)

--- src/test/java/com/example/gabojago_server/web/controller/member/MemberControllerTest.java
+++ src/test/java/com/example/gabojago_server/web/controller/member/MemberControllerTest.java
@@ -4,8 +4,14 @@ import com.example.gabojago_server.config.TestSecurityConfig;
 import com.example.gabojago_server.dto.request.member.ChangeNickNameRequestDto;
 import com.example.gabojago_server.dto.request.member.ChangePasswordRequestDto;
 import com.example.gabojago_server.dto.request.member.ChangePhoneRequestDto;
+import com.example.gabojago_server.dto.response.member.AlarmResponseDto;
 import com.example.gabojago_server.dto.response.member.MemberResponseDto;
 import com.example.gabojago_server.jwt.JwtTokenProvider;
+import com.example.gabojago_server.model.alarm.AlarmEntity;
+import com.example.gabojago_server.model.alarm.AlarmType;
+import com.example.gabojago_server.model.article.Article;
+import com.example.gabojago_server.model.member.Member;
+import com.example.gabojago_server.service.alarm.AlarmService;
 import com.example.gabojago_server.service.member.MemberService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -18,20 +24,28 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
-import org.springframework.security.core.parameters.P;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+import java.util.Map;
 
+import static com.example.gabojago_server.web.controller.restDocs.RestDocsUtils.pageableDocsWithContent;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,12 +63,15 @@ class MemberControllerTest {
     private MemberService memberService;
 
     @MockBean
+    private AlarmService alarmService;
+
+    @MockBean
     private JwtTokenProvider jwtTokenProvider;
 
     private ObjectMapper objectMapper;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
     }
@@ -84,7 +101,7 @@ class MemberControllerTest {
     @DisplayName("[POST] [/api/members/nickname] 회원 닉네임 변경")
     @WithMockUser(value = "1")
     public void changeMemberNicknameTest() throws Exception {
-        ChangeNickNameRequestDto request =  createChangeNicknameRequestDto();
+        ChangeNickNameRequestDto request = createChangeNicknameRequestDto();
 
         given(memberService.changeNickname(request.getEmail(), request.getNickname()))
                 .willReturn(createMemberResponseDto());
@@ -174,20 +191,91 @@ class MemberControllerTest {
                 ));
     }
 
-    private MemberResponseDto createMemberResponseDto(){
+    @Test
+    @DisplayName("[GET] [/api/member/alarms] 사용자 알람 요청")
+    @WithMockUser(value = "1")
+    public void getAlarmListTest() throws Exception {
+        Pageable pageable = PageRequest.of(0, 10);
+        given(alarmService.findAlarms(1L, pageable))
+                .willReturn(createSubAlarmResponseDto(pageable));
+
+        mockMvc.perform(get("/api/members/alarms")
+                        .param("page", String.valueOf(pageable.getPageNumber()))
+                        .param("size", String.valueOf(pageable.getPageSize()))
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer ${AccessToken}")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("getAlarmList"))
+                .andDo(document("member/info/alarm",
+                        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        requestParameters(
+                                parameterWithName("page").description("페이지"),
+                                parameterWithName("size").description("페이지 사이즈")),
+                        responseFields(
+                                pageableDocsWithContent(createPageAlarmResponseDtoResponseBody())
+                        )
+                ));
+    }
+
+    private Map<String, String> createPageAlarmResponseDtoResponseBody() {
+        return Map.of(
+                "content[].message", "메시지",
+                "content[].alarmType", "알람 타입 COMMENT , LIKE",
+                "content[].publisherNickname", "댓글 혹은 좋아요를 누른 사용자",
+                "content[].nickname", "게시글 주인 닉네임",
+                "content[].postId", "알람이 발생한 게시글 ID"
+        );
+    }
+
+    private Page<AlarmResponseDto> createSubAlarmResponseDto(Pageable pageable) {
+        AlarmResponseDto rseponse = AlarmResponseDto.from(createSubAlarmEntity(), createStubArticle(), createSubMember());
+        return new PageImpl<>(List.of(rseponse), pageable, 1);
+    }
+
+    private Article createStubArticle() {
+        return Article.builder()
+                .writer(createSubMember())
+                .title("stub Title")
+                .content("stub Content")
+                .build();
+    }
+
+    private AlarmEntity createSubAlarmEntity() {
+        return AlarmEntity.builder()
+                .alarmType(AlarmType.COMMENT)
+                .member(createSubMember())
+                .targetId(2L)
+                .publisherId(2L)
+                .build();
+    }
+
+    private Member createSubMember() {
+        return Member.builder()
+                .birth("2023-05-11")
+                .password("test@test!@#213")
+                .email("stub@gmail.com")
+                .nickname("stub")
+                .name("stub")
+                .build();
+    }
+
+
+    private MemberResponseDto createMemberResponseDto() {
         return MemberResponseDto.builder()
                 .email("test@test.com")
                 .nickname("test")
                 .build();
     }
 
-    private ChangeNickNameRequestDto createChangeNicknameRequestDto(){
+    private ChangeNickNameRequestDto createChangeNicknameRequestDto() {
         return new ChangeNickNameRequestDto(
                 "test@test.com", "test"
         );
     }
 
-    private ChangePasswordRequestDto createChangePasswordRequestDto(){
+    private ChangePasswordRequestDto createChangePasswordRequestDto() {
         return new ChangePasswordRequestDto(
                 "test@test.com", "test123!@#", "test1234!@#"
         );
