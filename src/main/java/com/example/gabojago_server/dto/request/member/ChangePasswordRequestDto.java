a8b287246e57161b08b133fa9d8c151f3f0df62a
syahn <496300@naver.com>
2023-05-16 22:50:47 +0900

    refactor: 비밀번호 특수문자 허용 범위 변경 (#21)
    - 특수문자가 일부만 허용 가능하도록 변경함
    
    #21

--- src/main/java/com/example/gabojago_server/dto/request/member/ChangePasswordRequestDto.java
+++ src/main/java/com/example/gabojago_server/dto/request/member/ChangePasswordRequestDto.java
@@ -17,6 +17,6 @@ public class ChangePasswordRequestDto {
     private String email;
 
     @NotBlank(message = "새로운 비밀번호를 입력하세요.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자의 구성입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])(?=\\\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자의 구성입니다.")
     private String newPassword;
 }
